345bf4d31d6cefb31e397e13895dbc93fb2e9662
abdo <abdo@example.com>
2025-12-07 15:08:27 +0100

    chore: entferne .eslintignore und füge ESLint-Konfiguration hinzu; aktualisiere Abhängigkeiten in package.json

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+        ReactDOM: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['sw.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.serviceworker,
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'content/img/**', 'content/fonts/**', 'pages/**/*.html', 'content/vendor/**'],
+  },
+];
